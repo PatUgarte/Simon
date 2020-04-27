3afe06217ad883ce7656e03b0523a782a1904cf9
Pat Ugarte <patriciomugarte@gmail.com>
2020-04-27 17:16:06 -0300

    Agregadoda la lógica para la secuencia, iluminación de los botones y otras mejoras en el diseño

--- simon-dice.js
+++ simon-dice.js
@@ -5,14 +5,62 @@ const rojo = document.getElementById("rojo");
 const azul = document.getElementById("azul");
 const botonEmpezar = document.getElementById("boton-empezar");
 
+const CANTIDAD_NIVELES = 10;
+
 class Juego {
-    constructor(){
+    constructor() {
         this.inicializarJuego();
+        this.generarSecuencia();
+        this.avanzarUnNivel();
     }
-    
-    inicializarJuego(){
+
+    inicializarJuego() {
         botonEmpezar.classList.add("hide");
         gameboard.classList.remove("hide");
+        this.nivel = 1;
+        this.colores = { verde, amarillo, rojo, azul };
+    }
+
+    generarSecuencia() {
+        this.secuencia = new Array(CANTIDAD_NIVELES).fill().map(() => Math.floor(Math.random() * 4));
+    }
+
+    avanzarUnNivel() {
+        setTimeout(() => this.iluminarSecuencia(), 500);
+    }
+
+    iluminarSecuencia() {
+        for (let i = 0; i < this.nivel; i++) {
+            let valor = this.secuencia[i];
+            let color = this.obtenerColorDe(valor);
+            setTimeout(() => this.iluminar(color), 1000 * i);
+        }
+    }
+
+    iluminar(unColor) {
+        let objetoColor = this.colores[unColor];
+        objetoColor.classList.add("light");
+        setTimeout(() => this.apagar(unColor), 250);
+    }
+
+    apagar(unColor) {
+        let objetoColor = this.colores[unColor];
+        objetoColor.classList.remove("light");
+    }
+
+    obtenerColorDe(numero) {
+        switch (numero) {
+            case 0:
+                return "verde";
+            case 1:
+                return "amarillo";
+            case 2:
+                return "rojo";
+            case 3:
+                return "azul";
+            default:
+                return "null";
+        }
     }
 }
 
